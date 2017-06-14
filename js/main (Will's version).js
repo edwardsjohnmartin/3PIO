7cd7acf1aa848a435c80303384ff234de81e08fb
Will Grigg <grigwill@isu.edu>
2017-06-14 09:20:41 -0600

    Added updated main.js script

---
+++ js/main (Will's version).js
@@ -0,0 +1,72 @@
+var test;             //This stores the TEST code that will be appended to the student code.
+    var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+        mode: {name: "python",
+               version: 2,
+               singleLineStringErrors: false},
+        lineNumbers: true,
+        indentUnit: 4,
+        matchBrackets: true,
+		theme: "solarized dark"
+    });
+
+
+  //editor.on('change', disableSubmitBtn);
+	document.getElementById("runButton").onclick = run;
+
+  window.onload = function() {
+        var fileInput = document.getElementById('fileInput');
+        var doc = document.getElementById('output');
+        fileInput.addEventListener('change', function(e) {
+            var file = fileInput.files[0];
+            var textType = /text.*/;
+
+            if (file.type.match(textType)) {
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    test = reader.result;
+                    mod = Sk.importMainWithBody("<stdin>", false, test, true);
+                    var runMethod = mod.tp$getattr('GET_PROMPT');
+                    var ret = Sk.misceval.callsim(runMethod);
+                    document.getElementById('prompt').innerHTML = ret.v;
+
+                }
+                reader.readAsText(file);
+            } else {
+                alert("File not supported!")
+            }
+        });
+  }
+function outf(text) {
+    var mypre = document.getElementById("output");
+    studentOut = mypre.innerHTML = mypre.innerHTML + text;
+}
+function builtinRead(x) {
+    if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+            throw "File not found: '" + x + "'";
+    return Sk.builtinFiles["files"][x];
+}
+
+function run() {
+  var mod;
+  var program = editor.getValue() + "\n" + test;
+  var outputArea = document.getElementById("output")
+  outputArea.innerHTML = '';
+  Sk.pre = "output";
+    Sk.configure({output:outf, read:builtinRead});
+  (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'mycanvas';
+   var myPromise = Sk.misceval.asyncToPromise(function() {
+      mod = Sk.importMainWithBody("<stdin>", false, program, true);
+      return mod;
+   });
+
+    myPromise.then(function(mod) {
+        console.log('success');
+    },
+        function(err) {
+          var line_num = Number(err.toString().split("on line", 2)[1]);
+        console.log(err.toString());
+    });
+    var runMethod = mod.tp$getattr('TEST');
+    var ret = Sk.misceval.callsim(runMethod, Sk.builtin.str(editor.getValue()), Sk.builtin.str(outputArea.innerHTML));
+    alert(ret.v);
+  }
