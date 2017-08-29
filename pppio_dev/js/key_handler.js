af286f75896b80b35fe13f84a0fe669624490b43
Joseph Valentin <valejose@isu.edu>
2017-08-29 13:00:37 -0600

    Code input fields are now focused on Load, unless a name field exists, then the name field will be focused instead.

--- pppio_dev/js/key_handler.js
+++ pppio_dev/js/key_handler.js
@@ -1,7 +1,12 @@
 window.onload = function() {
+  if(document.getElementsByName("name")[0] != undefined){
+    document.getElementsByName("name")[0].focus();
+  } else {
+    document.getElementsByTagName("textArea")[1].focus();
+  }
     $(window).keydown(function(event) {
         if(event.ctrlKey && event.keyCode == 13) { 
-          document.getElementById("runButton").click();
+          document.getElementById("runButton").click();   
           event.preventDefault(); 
         } 
         if(event.ctrlKey && event.keyCode == 191) {
