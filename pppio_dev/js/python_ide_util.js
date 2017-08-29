1db6338929c1a0ccdac85059fa6954b4c94d1382
John Edwards <edwardsjohnmartin@gmail.com>
2017-08-28 21:44:04 -0600

    Python editor now supports type() function. Moves some shared code into python_ide_util.js

---
+++ pppio_dev/js/python_ide_util.js
@@ -0,0 +1,13 @@
+function outf(text) { 
+	if (text && typeof(text) != "undefined") {
+		text = text.replace("<", "&lt;").replace(">", "&gt;");
+		console.log(text);
+		var mypre = document.getElementById("output"); 
+		console.log(mypre);
+		mypre.innerHTML = mypre.innerHTML + text;
+	}
+} 
+function inf(prompt) {
+	// Must copy the prompt string for some reason
+  return window.prompt(String(prompt));
+}
