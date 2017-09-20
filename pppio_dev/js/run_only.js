c55fe3a60e436e9507fda76278ef9fa8b5300c53
Joseph Valentin <valejose@isu.edu>
2017-09-20 17:31:03 -0600

    Fixed scrollbar in sandbox. Formatting fixes.

--- pppio_dev/js/run_only.js
+++ pppio_dev/js/run_only.js
@@ -1,26 +1,27 @@
 function betterTab(cm) {
-  if (cm.somethingSelected()) {
-    cm.indentSelection("add");
-  } else {
-    cm.replaceSelection(cm.getOption("indentWithTabs")? "\t":
-												Array(cm.getOption("indentUnit") + 1).join(" "),
-												"end", "+input");
-  }
+	if (cm.somethingSelected()) {
+		cm.indentSelection("add");
+	} else {
+		cm.replaceSelection(
+			cm.getOption("indentWithTabs") ? "\t" :
+			Array(cm.getOption("indentUnit") + 1).join(" "), "end", "+input");
+	}
 }
 
 var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-  mode: {name: "python",
-         version: 2,
-         singleLineStringErrors: false},
-  lineNumbers: true,
-  indentUnit: 4,
-  matchBrackets: true,
-	//theme: "solarized dark"
+	mode: {
+		name: "python",
+		version: 2,
+		singleLineStringErrors: false
+	},
+	lineNumbers: true,
+	indentUnit: 4,
+	matchBrackets: true,
 	theme: "default",
 	extraKeys: { Tab: betterTab }
 });
 
-document.getElementById("runButton").onclick = function() { clearAlerts(); run(); };
+document.getElementById("runButton").onclick = function () { clearAlerts(); run(); };
 
 /*
 uncomment to disable copy/paste for projects
@@ -29,23 +30,10 @@ editor.on('cut', function(a, e) {e.preventDefault();});
 editor.on('paste', function(a, e) {e.preventDefault();});
 */
 
-// function outf(text) { 
-// 	if (text && typeof(text) != "undefined") {
-// 		text = text.replace("<", "&lt;").replace(">", "&gt;");
-// 		console.log(text);
-// 		var mypre = document.getElementById("output"); 
-// 		console.log(mypre);
-// 		mypre.innerHTML = mypre.innerHTML + text;
-// 	}
-// } 
-// function inf(prompt) {
-// 	// Must copy the prompt string for some reason
-//   return window.prompt(String(prompt));
-// }
 function builtinRead(x) {
-    if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
-            throw "File not found: '" + x + "'";
-    return Sk.builtinFiles["files"][x];
+	if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+		throw "File not found: '" + x + "'";
+	return Sk.builtinFiles["files"][x];
 }
 
 function run() {
@@ -53,51 +41,41 @@ function run() {
 	var outputArea = document.getElementById("output");
 	outputArea.innerHTML = '';
 	Sk.pre = "output";
-  //Sk.configure({output:outf, read:builtinRead});
 	Sk.configure({
-		output:outf, read:builtinRead,
-		inputfun:inf,
-    // inputfun: function (prompt) {
-    //   return window.prompt(prompt);
-    // },
-    inputfunTakesPrompt:true,
-    // output: function(str) {
-    //   //strip out line-feeds
-    //   if (str.replace(/\n/g, "") !== "") {
-    //     repl.print(str);
-    //   }
-    // },
-    // read: function (x) {
-    //   if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined) {
-    //     throw "File not found: '" + x + "'";
-    //   }
-    //   return Sk.builtinFiles["files"][x];
-    // },
-		// retainglobals: true
+		output: outf,
+		read: builtinRead,
+		inputfun: inf,
+		inputfunTakesPrompt: true,
 	});
 	(Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'mycanvas';
-  var myPromise = Sk.misceval.asyncToPromise(function() {
-    return Sk.importMainWithBody("<stdin>", false, program, true);
-  });
-  myPromise.then(function(mod) {},
-								 function(err) {
-									 markError(err.toString());
-								 });
+	var myPromise = Sk.misceval.asyncToPromise(function () {
+		return Sk.importMainWithBody("<stdin>", false, program, true);
+	});
+	myPromise.then(function (mod) { },
+		function (err) {
+			markError(err.toString());
+		});
+
+	//Delays the function that creates the drawing area by 750ms to allow the other elements to be created by the time this code runs
+	//This will get the calculate the size the text output area needs to be by getting the total height of the right column and subtracting
+	//the size of the drawing area
+	setTimeout(function () {
+		var canvas = document.getElementById("mycanvas");
+		var canHeight = canvas.offsetHeight;
+		var canParHeight = canvas.parentNode.clientHeight;
+		outputArea.style.height = (canParHeight - canHeight) + "px";
+	}, 750);
 }
 var codeAlerts = document.getElementById('codeAlerts');
 
-function clearAlerts()
-{
+function clearAlerts() {
 	codeAlerts.innerHTML = '';
 }
 
-function markError(errorMessage)
-{
+function markError(errorMessage) {
 	codeAlerts.innerHTML += '<div class="alert alert-danger alert-dismissible mar-0" role="alert" id="infoAlert">' + errorMessage + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
 }
 
-function markSuccess(successMessage)
-{
+function markSuccess(successMessage) {
 	codeAlerts.innerHTML += '<div class="alert alert-success alert-dismissible mar-0" role="alert" id="infoAlert">' + successMessage + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
 }
-
--- pppio_dev/views/project/editor.php
+++ pppio_dev/views/project/editor.php
@@ -5,54 +5,45 @@ echo '<link rel="stylesheet" href="css/editor.css">';
 require_once('views/shared/CodeMirror.php');
 require_once('views/shared/Skulpt.php');
 
-echo '<div class="col-xs-12 height-100 flex-columns">';
-
-echo '<div class="row no-shrink">
+echo '<div class="col-xs-12 height-100 flex-columns">;
+	<div class="row no-shrink">
 		<div class="col-xs-12">
-			<h3>' . $project_props['name']  . '</h3>'; //$exercise_props['lesson']->value); //bugs leftover from switching to only one lesson per project
-			//check if empty
-			//if($exercise_props['name'] !== '') echo '<h4>' . htmlspecialchars($exercise_props['name']). '</h4>';
-
-			echo '<h4 class="panel-title"><a data-toggle="collapse" data-target="#description" href="#prompt">Description</a></h4><div id="description" class="collapse in"><p id="prompt">' . $project_props['description'] . '</p></div>
+			<h3>' . $project_props['name']  . '</h3>;
+			<h4 class="panel-title"><a data-toggle="collapse" data-target="#description" href="#prompt">Description</a></h4><div id="description" class="collapse in"><p id="prompt">' . $project_props['description'] . '</p></div>
 		</div>
-	</div>';
+	</div>;
 
-echo '<div class="row no-shrink navbar-default navbar-form navbar-left">
-					<button type="button" class="btn btn-default" id="runButton"><span class="glyphicon glyphicon-play" aria-hidden="true"></span><span class="sr-only">Run</span></button>';
-			//<span>Choose a test file:</span><input type="file"  class="form-control" id="fileInput">
-			echo '</div>
-			<div class="row no-shrink"> <!--this alert needs to be filled with the error, or the next button-->
-			<div class="col-xs-12 pad-0">
-				<div id="codeAlerts"></div>
-			</div>
+	<div class="row no-shrink navbar-default navbar-form navbar-left">
+		<button type="button" class="btn btn-default" id="runButton"><span class="glyphicon glyphicon-play" aria-hidden="true"></span><span class="sr-only">Run</span></button>;
+	</div>
+	<div class="row no-shrink"> <!--this alert needs to be filled with the error, or the next button-->
+		<div class="col-xs-12 pad-0">
+			<div id="codeAlerts"></div>
 		</div>
-			<div class="row overflow-hidden height-100">
-				<div class="col-xs-6 height-100 overflow-hidden pad-0">
-					<textarea id="code" name="code">';
-					if($code != null)
-					{
-						echo $code;
-					}
-					else
-					{
-						echo $project_props['starter_code'];
-					}
-					echo '</textarea>
-				</div>
-				<div class="col-xs-6 height-100">
-
-					<div id="mycanvas" class="graphicalOutput"></div>
-                    <div class="textOutput">
-      				  <pre id="output"></pre>
-                    </div>
-
-				</div>
+	</div>
+	<div class="row overflow-hidden height-100">
+		<div class="col-xs-6 height-100 overflow-hidden pad-0">
+			<textarea id="code" name="code">';
+				if($code != null)
+				{
+					echo $code;
+				}
+				else
+				{
+					echo $project_props['starter_code'];
+				}
+			echo '</textarea>
+		</div>
+		<div class="col-xs-6 height-100">
+			<div id="mycanvas" class="graphicalOutput"></div>
+            	<div class="textOutput">
+      				<pre id="output"></pre>
+            	</div>
 			</div>
-			';
-
-echo '</div></div>';
+		</div>;
+	</div>
+</div>';
 
 echo '<script>var concept_id = ' . $concept->get_id() . '; var readonly = ' . ($readonly ? 'true' : 'false') . ';</script>';
 echo '<script src="js/project_editor.js"></script>';
-
 ?>
