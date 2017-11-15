9658221356c5b5551623a75ef3c5d0d890928500
John Edwards <edwardsjohnmartin@gmail.com>
2017-11-15 14:57:35 -0700

    Fixed importer bug when file is thought to be c++

--- pppio_dev/controllers/exam_controller.php
+++ pppio_dev/controllers/exam_controller.php
@@ -169,7 +169,7 @@
 						$finfo = new finfo(FILEINFO_MIME_TYPE);
 						if (false === $ext = array_search(
                                 $finfo->file($_FILES['file']['tmp_name']),
-							array('text/plain', 'text/x-fortran', 'text/x-python'),
+							array('text/plain', 'text/x-c++', 'text/x-fortran', 'text/x-python'),
 							true
 						)) {
 							add_alert('Invalid file format: '.$finfo->file($_FILES['file']['tmp_name']), Alert_Type::DANGER);
