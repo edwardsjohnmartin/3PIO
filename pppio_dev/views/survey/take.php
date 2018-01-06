9f0279373a94d7e6ca2d027c03d65a842719f24f
Joseph Valentin <valejose@isu.edu>
2018-01-06 16:36:49 -0700

    Required Tag on Surveys
    Added the 'required' tag to the input elements used for surveys. This makes the browser for them to answer before they can submit a survey. This is also nice because for range questions, the browser will force their answer to be within the correct range before they can submit.

--- pppio_dev/views/survey/take.php
+++ pppio_dev/views/survey/take.php
@@ -13,30 +13,41 @@ $questions = $survey_questions;
 		echo '<label class="input-md">' . $question_index . '. ' . $question_props['prompt'] . '</label>';
 
 		if($question_props['survey_question_type'] == Question_Type_Enum::MULTIPLE_CHOICE){
+			$ele_name = 'm_' . $q_id;
+
 			echo '<div class="radio">';
-			$q = 'm_' . $q_id;
 			foreach($question_props['survey_choices'] as $c_id => $c_value){
 				echo '<label class="control-label radio-inline input-md">';
 
-				if(array_key_exists($q, $_POST) and $_POST[$q] == $c_key){
-					$end = ' checked="checked">';
+				if(array_key_exists($ele_name, $_POST) and $_POST[$ele_name] == $c_key){
+					$end = ' checked="checked" required>';
 				} else {
-					$end = '>';
+					$end = ' required>';
 				}
 
-				echo '<input class="radio" value="' . $c_id . '" type="radio" name="' . $q . '"' . $end;
+				echo '<input class="radio" value="' . $c_id . '" type="radio" name="' . $ele_name . '"' . $end;
 				echo $c_value['choice'] . '</label>';
 			}
 			echo '</div>';
 		} else if($question_props['survey_question_type'] == Question_Type_Enum::RANGE){
 			$ele_name = 'r_' . $q_id;
+
 			echo '<div>';
-			echo '<input type="number" placeholder="#" name="' . $ele_name . '" min="'.$question_props['min'].'" max="'.$question_props['max'].'"/>';
+			echo '<input type="number" placeholder="#" name="' . $ele_name . '" min="'.$question_props['min'].'" max="'.$question_props['max'].'"';
+			if(array_key_exists($ele_name, $_POST)){
+				echo ' value="' . intval($_POST[$ele_name]) . '"';
+			}
+			echo ' required/>';
 			echo '</div>';
 		} else if($question_props['survey_question_type'] == Question_Type_Enum::SHORT_ANSWER){
 			$ele_name = 's_' . $q_id;
+
 			echo '<div>';
-			echo '<input type="text" placeholder="Enter your answer here" name="' . $ele_name . '"/>';
+			echo '<input type="text" placeholder="Enter your answer here" name="' . $ele_name . '"';
+			if(array_key_exists($ele_name, $_POST)){
+				echo ' value="' . $_POST[$ele_name] . '"';
+			}
+			echo ' required/>';
 			echo '</div>';
 		}
 		echo '</div>';
