2b80698d0975cf9c88e170e61d7ee3bc66a0c749
Joseph Valentin <valejose@isu.edu>
2018-01-28 21:45:10 -0700

    Confirmation Dialog for Complete Exercises
    Added a confirmation dialog to completing exercises for a user

--- pppio_dev/js/admin_functions.js
+++ pppio_dev/js/admin_functions.js
@@ -1,19 +1,24 @@
 ﻿function complete_previous_exercises(ex_id, les_id, con_id, u_id, btn_id) {
-    $.ajax({
-        method: "POST",
-        url: "?controller=concept&action=complete_exercises_ajax",
-        data: { exercise_id: ex_id, lesson_id: les_id, concept_id: con_id, user_id: u_id }
-    });
+    var conf = confirm("Do you complete all exercises in the section up to the exercise you selected?");
 
-    var i = 1;
-    var btn_num = parseInt(btn_id.substring(9)); //length of "btn_tile_"
+    if (conf) {
 
-    for (i = 1; i <= btn_num; i++) {
-        var btn_id_temp = "btn_tile_" + i.toString();
-        var btn = document.getElementById("btn_tile_" + i.toString());
-        btn.classList.remove("btn-default");
-        btn.classList.remove("btn-success");
+        $.ajax({
+            method: "POST",
+            url: "?controller=concept&action=complete_exercises_ajax",
+            data: { exercise_id: ex_id, lesson_id: les_id, concept_id: con_id, user_id: u_id }
+        });
 
-        btn.classList.add("btn-success");
+        var i = 1;
+        var btn_num = parseInt(btn_id.substring(9)); //length of "btn_tile_"
+
+        for (i = 1; i <= btn_num; i++) {
+            var btn_id_temp = "btn_tile_" + i.toString();
+            var btn = document.getElementById("btn_tile_" + i.toString());
+            btn.classList.remove("btn-default");
+            btn.classList.remove("btn-success");
+
+            btn.classList.add("btn-success");
+        }
     }
 }
